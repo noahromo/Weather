2dc042b630c6a073c17d754f2aa30db985de87e3
Noah Romo <noahromo17@gmail.com>
2021-04-20 01:55:17 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Noah Romo on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
